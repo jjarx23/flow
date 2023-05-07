400aa8a5f3760a37547f3247b4d44f1117f9885b
Jude <judeolaolu147@yahoo.com>
2023-05-07 23:47:40 +0100

    added version and restructured flow brigde struct

--- Flow.h
+++ Flow.h
@@ -1,35 +1,41 @@
 #ifndef FLOW_H
 #define FLOW_H
 
+#define FLOW_VERSION_MAJOR 0
+#define FLOW_VERSION_MINOR 2
+
 #ifdef __cplusplus
 extern 'C'
 {
 #endif
 
-    typedef struct Flow
+    struct FlowOp
     {
-        void *zero;
-        void *one;
-        int (*stringOf)(const void *, char *, size_t);
-        void *(*add)(void *, void *);
-        void *(*multiply)(void *, void *);
-        void *(*divide)(void *, void *);
-        void *(*subtract)(void *, void *);
-        void *(*power)(void *, void *);
-    } *Flow_t;
-    typedef struct DFlow
+        void *(*add)(void *, void *, ...);
+        void *(*multiply)(void *, void *, ...);
+        void *(*divide)(void *, void *, ...);
+        void *(*subtract)(void *, void *, ...);
+        void *(*power)(void *, void *, ...);
+    };
+    struct Flow_st
     {
-        void *(*add)(void *, void *, void *);
-        void *(*multiply)(void *, void *, void *);
-        void *(*divide)(void *, void *, void *);
-        void *(*subtract)(void *, void *, void *);
-        void *(*power)(void *, void *, void *);
-    } *DFlow_t;
+        const int V_MAJOR_NUMBER, V_MINOR_NUMBER;
+        struct
+        {
+            void *zero;
+            void *one;
+        } constants;
+        struct
+        {
+            int (*stringOf)(const void *, char *, size_t);
+            void *(*fill)(int rank, int *shape, double d);
+        } util;
+        struct FlowOp math;
+        struct FlowOp deriv;
+    };
 
-    extern const Flow_t Flow;
-    extern const DFlow_t DFlow;
-    // other declarations go here
-
+    extern const struct Flow_st *Flow_;
+#define Flow (*Flow_)
 #ifdef __cplusplus
 }
 #endif
