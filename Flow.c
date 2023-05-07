feed46f071e09b5af1a5004cd6dbdfb4237115f0
Jude <judeolaolu147@yahoo.com>
2023-05-07 23:48:40 +0100

    modified flow struct

--- Flow.c
+++ Flow.c
@@ -3,7 +3,5 @@
 #include "Flow.h"
 #include "Flow.r.h"
 
-struct Flow flow = {0};
-const Flow_t Flow = &flow;
-struct DFlow dflow = {0};
-const DFlow_t DFlow = &dflow;
+struct Flow_st flow = {FLOW_VERSION_MAJOR, FLOW_VERSION_MINOR};
+const struct Flow_st *Flow_ = &flow;
